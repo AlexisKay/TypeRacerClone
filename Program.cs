0fbc9314e14015e99dc61cd3fae795f5fda19ee5
ALEXISPC1-1LR7S\Alexis <alexiskay1998@gmail.com>
2024-01-28 22:01:32 -0700

    added title, small commentary

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using TypeRacerClone.Helpers;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        #region Title
+
+        Console.Title = "TypeRacerClone - By AlexisKay.";
+        ConsoleHelpers.CenterHeader("" +
+            "\r\n████████╗██╗░░░██╗██████╗░███████╗██████╗░░█████╗░░█████╗░███████╗██████╗░" +
+            "\r\n╚══██╔══╝╚██╗░██╔╝██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗" +
+            "\r\n░░░██║░░░░╚████╔╝░██████╔╝█████╗░░██████╔╝███████║██║░░╚═╝█████╗░░██████╔╝" +
+            "\r\n░░░██║░░░░░╚██╔╝░░██╔═══╝░██╔══╝░░██╔══██╗██╔══██║██║░░██╗██╔══╝░░██╔══██╗" +
+            "\r\n░░░██║░░░░░░██║░░░██║░░░░░███████╗██║░░██║██║░░██║╚█████╔╝███████╗██║░░██║" +
+            "\r\n░░░╚═╝░░░░░░╚═╝░░░╚═╝░░░░░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚══════╝╚═╝░░╚═╝");
+
+        ConsoleHelpers.CenterLines("A TypeRacer clone, by AlexisKay \r\n");
+        #endregion 
+
+        ConsoleHelpers.CenterLines("Enter Your Name: \r\n");
+        string? userName = Console.ReadLine();
+
+        ConsoleHelpers.CenterLines($"Welcome, {userName}, Ready to play? \r\n");
+    }
+}
