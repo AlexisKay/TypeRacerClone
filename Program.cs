e8307d0596dbbeb47e9466a5e6bc30023f105522
ALEXISPC1-1LR7S\Alexis <alexiskay1998@gmail.com>
2024-01-31 12:19:46 -0700

    added console line remover method

--- Program.cs
+++ Program.cs
@@ -22,6 +22,24 @@ public class Program
         ConsoleHelpers.CenterLines("Enter Your Name: \r\n");
         string? userName = Console.ReadLine();
 
+        ClearConsoleLines(3);
         ConsoleHelpers.CenterLines($"Welcome, {userName}, Ready to play? \r\n");
+
+
+        //Delete name entry before welcome line displays
+        //pop up menus in box 
+
+        ConsoleHelpers.CenterLines("ENTER to play", "BACKSPACE to see scores", "ESC to exit");
+
+    }
+
+    public static void ClearConsoleLines(int lines = 1)
+    {
+        for(int i = 0; i < lines; i++)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.WriteLine(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
     }
 }
