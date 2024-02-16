bbec6cf6513d8b8f76516d0ff0b8c857dbc4daa3
ALEXISPC1-1LR7S\Alexis <alexiskay1998@gmail.com>
2024-02-15 22:29:11 -0700

    added more logic to game..getting closer

--- MenuPages/MainGame.cs
+++ MenuPages/MainGame.cs
@@ -15,10 +15,14 @@ namespace TypeRacerClone.MenuPages
         public int PlayerError;
         public int Wpm;
         public TimeSpan TimeElapsed;
-        public System.Timers.Timer CountdownTimer;
+        public TimeSpan CountdownTimer;
         private bool gameStarted;
         public static KeyValuePair<string, string> randomEntry;
         const int PADDING = 12;
+        private List<char> userInputchars = new List<char>();
+        private int ErrorCount = 0;
+        private bool isFirstStart;
+
         public void InitializeGame()
         {
             PlayerScore = 0;
@@ -28,9 +32,9 @@ namespace TypeRacerClone.MenuPages
 
             //decide on timer to start and  count down?
             TimeElapsed = TimeSpan.FromSeconds(60);
-            CountdownTimer = new System.Timers.Timer();
-            CountdownTimer.Elapsed += new ElapsedEventHandler(Countdown);
-            CountdownTimer.Interval = 1000;
+            //CountdownTimer = new System.Timers.Timer();
+            //CountdownTimer.Elapsed += new ElapsedEventHandler(Countdown);
+            //CountdownTimer.Interval = 1000;
            
             ParseTxt();
 
@@ -105,17 +109,35 @@ namespace TypeRacerClone.MenuPages
              */
 
             //timer start TODO: not working correctly
-            CountdownTimer.Start();
-            gameStarted = true;
+            //CountdownTimer.Start();
+            if(gameStarted == false)
+                gameStarted = true;
 
             if (TimeElapsed < TimeSpan.FromSeconds(10))
                 Console.ForegroundColor = ConsoleColor.Red;
 
             Console.ResetColor();
 
-            //Delete text from before
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            ConsoleHelpers.ClearConsoleLines(3);
+            if (isFirstStart)
+            {
+                //Delete text from before
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                ConsoleHelpers.ClearConsoleLines(3);
+
+            }
+            else if (!isFirstStart)
+            {
+                Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
+                Console.Write("Press ESC to quit game.");
+                Console.SetCursorPosition(Console.WindowWidth - PADDING, Console.CursorTop + 1);
+                Console.WriteLine($"ACCURACY: {PlayerScore}");
+                Console.SetCursorPosition(Console.WindowWidth - PADDING, Console.CursorTop);
+                Console.WriteLine($"ERRORS: {PlayerError}");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                ConsoleHelpers.CenterLines(TimeElapsed.ToString() + "\r\n");
+                Console.ResetColor();
+            }
 
             //randomize parsed prompts:
             Random rand = new Random();
@@ -123,39 +145,52 @@ namespace TypeRacerClone.MenuPages
             string prompt = randomEntry.Key;
             string quote = "- " + randomEntry.Value;
 
-            Console.WriteLine("\r\n"+ prompt + "\r\n");
+            Console.WriteLine("\r\n" + prompt + "\r\n");
             ConsoleHelpers.CenterLines(quote);
-
 
             //read user inputs here?
             ConsoleKeyInfo userInput;
-            List<char> userInputchars = new List<char>();
             char[] promptArr = randomEntry.Key.ToCharArray();
 
-            int counter = 0;
+            int index = 0;
 
             do
             {
                 userInput  = Console.ReadKey(true);
 
-                userInputchars.Add(userInput.KeyChar);
-               
-                int index = userInputchars.IndexOf(userInput.KeyChar);
+                if(userInput.Key != ConsoleKey.Backspace)
+                {
+                    userInputchars.Add(userInput.KeyChar);
 
+                    index = userInputchars.IndexOf(userInput.KeyChar);
+
+                    if (promptArr[index] == userInputchars[index])
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(userInput.KeyChar);
+                    }
+                    else if (promptArr[index] != userInputchars[index])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(userInput.KeyChar);
+                        ErrorCount++;
+                    }
+                }
+                    
                 if (userInput.Key == ConsoleKey.Backspace)
                 {
-                    //remove last item?
-                }
+                    //works!
+                    if(index > 0)
+                    {
+                        userInputchars.Remove(userInputchars[index]);
+                        Console.Write(userInput.KeyChar);
+                        Console.Write(' ');
+                        Console.Write(userInput.KeyChar);
+                        index--;
 
-                if (promptArr[index] == userInputchars[index])
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write(userInput.KeyChar);
-                }
-                else if(promptArr[index] != userInputchars[index])
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write(userInput.KeyChar);
+                        //check array alignment?
+                    }
+
                 }
 
             } while (userInput.Key != ConsoleKey.Enter);
@@ -175,10 +210,23 @@ namespace TypeRacerClone.MenuPages
                 {
                     case ConsoleKey.Enter:
                         if (gameStarted == false)
+                        {
+                            isFirstStart = true;
                             StartGame();
+                        }
                         else if (gameStarted)
                         {
+                            isFirstStart = false;
                             //next prompt?
+                            if(CountdownTimer.Seconds >= 0)
+                            {
+                                //generate new prompt
+                                AddScore();
+                                Console.Clear();
+                                //redraw screen
+                                StartGame();
+                                
+                            }
                         }
                         break;
 
@@ -220,11 +268,48 @@ namespace TypeRacerClone.MenuPages
 
                 }
             }
-
-
-
         }
         
+        public void GeneratePrompt()
+        {
+            Random rand = new Random();
+            randomEntry = promptDict.ElementAt(rand.Next(0, promptDict.Count));
+            string prompt = randomEntry.Key;
+            string quote = "- " + randomEntry.Value;
+
+            Console.WriteLine("\r\n" + prompt + "\r\n");
+            ConsoleHelpers.CenterLines(quote);
+        }
+
+        public void AddScore()
+        {
+            /* *WPM:
+             * Total Keys pressed / 5 = total number of words (5 because of avg word size)
+             * WPM = Total number of words / time elapsed Rounded down
+             *
+             *Accuracy Score: 
+             *  Score % = (Correct keys/Total keys) * 100
+             * 
+             * AWPM: Adjusted WPM - wpm adjusted for errors made
+             *  WPM * Accuracy (rounded down) 
+             *  **make this overall score?*/
+
+            if (CountdownTimer.Seconds > 0)
+            {
+                var totalWords = (userInputchars.Count) / 5;
+                var _wpm = (totalWords / 60) * 100;
+
+                //player score can be errors noted compared to keys pressed
+                //but for now....
+                var _playerScore = ((userInputchars.Count - ErrorCount) / userInputchars.Count) * 100;
+
+                if (gameStarted)
+                {
+                    PlayerScore += _playerScore;
+                    Wpm += _wpm;
+                }
+            }
+        }
        
     }
 }
